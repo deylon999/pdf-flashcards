a188f7ed4f409768c51fc852577f017ee106126c
deylon999 <86426285+deylon999@users.noreply.github.com>
2026-09-18 23:36:04 +0300

    Переворот карточки по клику через useState

--- src/components/CardItem.tsx
+++ src/components/CardItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import type { Card } from '../types';
 
 type Props = {
@@ -5,10 +6,16 @@ type Props = {
 };
 
 export function CardItem({ card }: Props) {
+  const [flipped, setFlipped] = useState(false);
+  const [isLearned, setIsLearned] = useState(card.learned)
+
   return (
-    <div>
-      <h3>{card.question}</h3>
-      <p>{card.answer}</p>
+    <div onClick={() => setFlipped(!flipped)}>
+      <h3>{flipped ? card.answer : card.question}</h3>
+      <p>{flipped ? 'Ответ' : 'Вопрос'}</p>
+
+      <button onClick={(e) => { e.stopPropagation(); setIsLearned(!isLearned)}}>Выучено</button>
+      <p>{isLearned ? 'Выучено' : 'Не выучено'}</p>
     </div>
   );
 }

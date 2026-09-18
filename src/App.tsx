0ba9a621abc45f2a950e378ce5255c1c176c8f0b
deylon999 <86426285+deylon999@users.noreply.github.com>
2026-09-18 21:40:55 +0300

    add readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
 export function App() {
-  return <h1>Карточки</h1>;
+  const name = 'Ваше имя';
+  const age = 25;
+  const movies = ['Матрица', 'Интерстеллар', 'Бойцовский клуб', 'Начало'];
+
+  return (
+    <div>
+      <h1>Карточки</h1>
+      <p>Меня зовут: {name}</p>
+      <p>Через 10 лет мне будет: {age+10}</p>
+      <ul>
+        {movies.map(movie => <li key={movie}>{movie}</li>)}
+      </ul>
+      <p>{age >= 18 ? 'Совершеннолетний' : 'Несовершеннолетний'}</p>
+    </div>
+  );
 }

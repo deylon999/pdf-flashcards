11ee346a609b084f1673b1e4115841e6d148f625
deylon999 <86426285+deylon999@users.noreply.github.com>
2026-09-19 13:31:11 +0300

    добавлен mui

---
+++ src/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: { main: '#3f51b5' },
+    background: { default: '#f5f5f5' },
+  },
+  typography: {
+    fontFamily: 'Roboto, Arial, sans-serif',
+  },
+});
